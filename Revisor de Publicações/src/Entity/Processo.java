955a1e2c814e642a5805dc086ca2d46e4696eb39
Víctor Vaz <victor-vaz@hotmail.com>
2015-02-03 11:01:33 -0200

    Entidade Processo

---
+++ Revisor de Publicações/src/Entity/Processo.java
@@ -0,0 +1,50 @@
+package Entity;
+
+import java.util.Date;
+
+/**
+ * Entidade processo
+ * @author Víctor Vaz de Oliveira <victor.vaz@vistaes.com.br>
+ */
+public class Processo
+{
+    /**
+     * Número do processo.
+     */
+    private int NumProcesso;
+    
+    /**
+     * Data da publicação.
+     */
+    private Date DataPublicacao;
+    
+    /**
+     * Escritório da publicação.
+     */
+    private Escritorio Escritorio;
+    
+    /**
+     * Vara da publicação.
+     */
+    private String Vara;
+    
+    /**
+     * Tribunal da publicação.
+     */
+    private Tribunal Tribunal;
+    
+    /**
+     * Arquivo da publicação.
+     */
+    private String Arquivo;
+    
+    /**
+     * Ordem da publicação.
+     */
+    private int Ordem;
+    
+    /**
+     * Número do processo.
+     */
+    private String NumeroProcesso;
+}
