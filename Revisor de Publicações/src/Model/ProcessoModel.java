8f3a72867900b04cd3776dd5317d4bc49ac04a42
Víctor Vaz <victor-vaz@hotmail.com>
2015-02-05 10:34:29 -0200

    Funções para buscar processos

--- Revisor de Publicações/src/Model/ProcessoModel.java
+++ Revisor de Publicações/src/Model/ProcessoModel.java
@@ -256,4 +256,156 @@ public class ProcessoModel
         
         return null;
     }
+    
+    /**
+     * Função para buscar os processos
+     * @param cRecorte
+     * @param cEstado
+     * @param dataBusca
+     * @param trechoBusca
+     * @return Processo
+     */
+    public List<Processo> buscar(Recorte cRecorte, Estado cEstado, Date dataBusca, String trechoBusca)
+    {
+        try
+        {
+            List<Processo> ListaProcessos = new ArrayList<>();
+            
+            String tabelaEstado = cTabelaEstadoModel.buscarTabelaPorEstado(cRecorte, cEstado).getNomeTabela();
+            
+            List<Tribunal> ListaTribunais = cTribunalModel.buscarPorEstado(cRecorte, cEstado);
+            
+            RecorteDAL DAL = new RecorteDAL();
+            DAL.setRecorte(cRecorte);            
+
+            String sql = "     SELECT P1.NUM      AS NUM_PUBLICACAO,"
+                       + "            P1.DATA     AS DATA_PUBLICACAO,"
+                       + "            P1.TRIBUNAL AS TRIBUNAL,"
+                       + "            P1.NOME     AS NOME_BUSCADO,"
+                       + "            E.NOME      AS NOME_ESCRITORIO"
+                       + "       FROM " + tabelaEstado + " P1"
+                       + " INNER JOIN " + tabelaEstado + "2 P2"
+                       + "         ON P1.NUM = P2.NUM2"
+                       + " INNER JOIN ESCRITORIO E"
+                       + "         ON E.CODIGO = P1.CODIGO"
+                       + "      WHERE P1.DATA = '" + (new SimpleDateFormat("yyyy-MM-dd").format(dataBusca)) + "'"
+                       + "        AND P2.PUBLICACAO LIKE '%" + trechoBusca + "%'";
+
+            ResultSet row = DAL.executarSelectQuery(sql);
+
+            while (row.next())
+            {
+                Processo cProcesso = new Processo();
+                cProcesso.setNumProcesso(row.getInt("NUM_PUBLICACAO"));
+                cProcesso.setDataPublicacao(row.getDate("DATA_PUBLICACAO"));
+                
+                // Pega o tribunal correspondente:
+                String nomeTribunal = row.getString("TRIBUNAL");
+                for (Tribunal cTribunal : ListaTribunais)
+                {
+                    if (cTribunal.getNomeTribunal().equals(nomeTribunal))
+                    {
+                        cProcesso.setTribunal(cTribunal);
+                    }
+                }
+                
+                // Escritório:
+                Escritorio cEscritorio = new Escritorio();
+                cEscritorio.setNome(row.getString("NOME_ESCRITORIO"));
+                
+                Cliente cCliente = new Cliente();
+                cCliente.setNome(row.getString("NOME_BUSCADO"));
+                cEscritorio.setCliente(cCliente);
+                cProcesso.setEscritorio(cEscritorio);
+                
+                ListaProcessos.add(cProcesso);
+            }
+
+            DAL.desconectar();
+            return ListaProcessos;
+        }
+        catch (SQLException ex)
+        {
+            new Excecao("Erro ao buscar os processos", this.getClass().getName(), ex.toString());
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Função para buscar os processos
+     * @param cRecorte
+     * @param cEstado
+     * @param dataBusca
+     * @param cTribunal
+     * @param trechoBusca
+     * @return Processo
+     */
+    public List<Processo> buscar(Recorte cRecorte, Estado cEstado, Date dataBusca, Tribunal cTribunal, String trechoBusca)
+    {
+        try
+        {
+            List<Processo> ListaProcessos = new ArrayList<>();
+            
+            String tabelaEstado = cTabelaEstadoModel.buscarTabelaPorEstado(cRecorte, cEstado).getNomeTabela();
+            
+            List<Tribunal> ListaTribunais = cTribunalModel.buscarPorEstado(cRecorte, cEstado);
+            
+            RecorteDAL DAL = new RecorteDAL();
+            DAL.setRecorte(cRecorte);            
+
+            String sql = "     SELECT P1.NUM      AS NUM_PUBLICACAO,"
+                       + "            P1.DATA     AS DATA_PUBLICACAO,"
+                       + "            P1.TRIBUNAL AS TRIBUNAL,"
+                       + "            P1.NOME     AS NOME_BUSCADO,"
+                       + "            E.NOME      AS NOME_ESCRITORIO"
+                       + "       FROM " + tabelaEstado + " P1"
+                       + " INNER JOIN " + tabelaEstado + "2 P2"
+                       + "         ON P1.NUM = P2.NUM2"
+                       + " INNER JOIN ESCRITORIO E"
+                       + "         ON E.CODIGO = P1.CODIGO"
+                       + "      WHERE P1.DATA = '" + (new SimpleDateFormat("yyyy-MM-dd").format(dataBusca)) + "'"
+                       + "        AND P1.TRIBUNAL = '" + cTribunal.getNomeTribunal() + "'"
+                       + "        AND P2.PUBLICACAO LIKE '%" + trechoBusca + "%'";
+
+            ResultSet row = DAL.executarSelectQuery(sql);
+
+            while (row.next())
+            {
+                Processo cProcesso = new Processo();
+                cProcesso.setNumProcesso(row.getInt("NUM_PUBLICACAO"));
+                cProcesso.setDataPublicacao(row.getDate("DATA_PUBLICACAO"));
+                
+                // Pega o tribunal correspondente:
+                String nomeTribunal = row.getString("TRIBUNAL");
+                for (Tribunal cTribunalLista : ListaTribunais)
+                {
+                    if (cTribunalLista.getNomeTribunal().equals(nomeTribunal))
+                    {
+                        cProcesso.setTribunal(cTribunalLista);
+                    }
+                }
+                
+                // Escritório:
+                Escritorio cEscritorio = new Escritorio();
+                cEscritorio.setNome(row.getString("NOME_ESCRITORIO"));
+                
+                Cliente cCliente = new Cliente();
+                cCliente.setNome(row.getString("NOME_BUSCADO"));
+                cEscritorio.setCliente(cCliente);
+                cProcesso.setEscritorio(cEscritorio);
+                
+                ListaProcessos.add(cProcesso);
+            }
+
+            DAL.desconectar();
+            return ListaProcessos;
+        }
+        catch (SQLException ex)
+        {
+            new Excecao("Erro ao buscar os processos", this.getClass().getName(), ex.toString());
+        }
+        
+        return null;
+    }
 }
